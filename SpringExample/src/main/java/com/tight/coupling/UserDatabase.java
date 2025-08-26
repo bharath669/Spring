07cda95ef94930622c0eb89c79b1d314e8c1604d
“bharath669” <vvbharath2006@gmail.com>
2025-08-26 18:40:48 +0100

    Tight coupling

---
+++ SpringExample/src/main/java/com/tight/coupling/UserDatabase.java
@@ -0,0 +1,8 @@
+package com.tight.coupling;
+
+public class UserDatabase {
+    public String getUserDetails(){
+//        Direct access from Database
+        return "User details from the Database";
+    }
+}
