4cacdc80c5628979c29e73d2b7969b2acce7d400
“bharath669” <vvbharath2006@gmail.com>
2025-08-28 19:46:05 +0100

    Setting up spring project

---
+++ SpringExample/src/main/java/com/car/bean/MyBean.java
@@ -0,0 +1,19 @@
+package com.car.bean;
+
+public class MyBean {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    public void showMessage(String message){
+        System.out.println("Message:"+ message);
+    }
+
+    @Override
+    public String toString() {
+        return "MyBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
