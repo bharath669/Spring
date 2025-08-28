4e90099568fb7647061ce9a7c44ac5c6bd9675bd
“bharath669” <vvbharath2006@gmail.com>
2025-08-28 17:50:05 +0100

    Loose coupling-2

--- SpringExample/src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ SpringExample/src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -5,5 +5,13 @@ public class LooseCouplingExample {
         UserDataProvider dataProvider=new UserDatabaseProvider();
         UserManager userManagerWithDb=new UserManager(dataProvider);
         System.out.println(userManagerWithDb.getUserInfo());
+
+        UserDataProvider webDataProvider=new WebServiceDataProvider();
+        UserManager userManagerWithWS=new UserManager(webDataProvider);
+        System.out.println(userManagerWithWS.getUserInfo());
+
+        UserDataProvider newDatabaseDataProvider=new NewDatabaseProvider();
+        UserManager userManagerWithNewDB=new UserManager(newDatabaseDataProvider);
+        System.out.println(userManagerWithNewDB.getUserInfo());
     }
 }
