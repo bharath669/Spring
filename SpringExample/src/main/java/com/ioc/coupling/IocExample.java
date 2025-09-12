4fea1252826e8341b791f316d88e44ddf5c145c9
“bharath669” <vvbharath2006@gmail.com>
2025-09-12 19:34:38 +0100

    autowire by name

--- SpringExample/src/main/java/com/ioc/coupling/IocExample.java
+++ SpringExample/src/main/java/com/ioc/coupling/IocExample.java
@@ -9,11 +9,9 @@ public class IocExample {
         UserManager userManagerWithDb=(UserManager) context.getBean("userManagerWithUserDatabaseProvider");
         System.out.println(userManagerWithDb.getUserInfo());
 
-//        ApplicationContext context1=new ClassPathXmlApplicationContext("applicationForIocLooseCoupling.xml");
         UserManager userManagerWithWS=(UserManager) context.getBean("userManagerWithNewDatabaseProvider");
         System.out.println(userManagerWithWS.getUserInfo());
 
-//        ApplicationContext context2=new ClassPathXmlApplicationContext("applicationForIocLooseCoupling.xml");
         UserManager userManagerWithNewDB=(UserManager)context.getBean ("userManagerWithWebServiceDatabaseProvider");
         System.out.println(userManagerWithNewDB.getUserInfo());
     }
